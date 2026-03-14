529e690cc876df6f673b6fe7a38ddf41dfde1ae2
24guptas2-ops <24guptas_2@rbunagpur.in>
2026-03-14 23:18:11 +0530

    added funtion to Generate Sample Data

--- reconcile.py
+++ reconcile.py
@@ -4,4 +4,19 @@ from faker import Faker
 import random
 from datetime import datetime
 import os
+def generate_sample_ledgers():
+    print("Generating sample ledger data...")
+    fake = Faker()
+    if not os.path.exists('data'):
+        os.makedirs('data')
 
+    num_base_transactions = 100
+    data = []
+    for i in range(num_base_transactions):
+        data.append({
+            'TransactionID':f'TXN{1000+i}',
+            'Date':fake.date_between(start_date='-30d',end_date='today'),
+            'Description':fake.catch_phrase(),
+            'Amount':round(random.uniform(10.50,500.99),2)
+        })
+    base_df = pd.DataFrame(data)
